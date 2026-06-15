4bd614e88e110510c431e856d04a27ac91c27c00
SemyonKvakin <sema.kvakin06@yandex.ru>
2026-06-16 01:25:14 +0300

    upload app py изменен подход разработки

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, url_for
+from database import engine, Base
+from blueprints.auth_bp     import auth_bp
+from blueprints.projects_bp import projects_bp
+from blueprints.datasets_bp import datasets_bp
+from blueprints.metrics_bp  import metrics_bp
+from blueprints.admin_bp    import admin_bp
+
+Base.metadata.create_all(bind=engine)
+
+app = Flask(__name__)
+app.secret_key = "metrics_flask_secret_2024"
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(projects_bp)
+app.register_blueprint(datasets_bp)
+app.register_blueprint(metrics_bp)
+app.register_blueprint(admin_bp)
+
+
+@app.route("/")
+def root():
+    return redirect(url_for("projects.index"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
